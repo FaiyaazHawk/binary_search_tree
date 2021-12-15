3b3dbf362cada2cdfdb95eb2f05e633a9bf50b41
FaiyaazHawk <faiyaaz.haque90@gmail.com>
2021-12-14 22:55:09 -0500

    made insert function

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,5 +1,5 @@
 class Node
-    attr_accessor data:, :left, :right
+    attr_accessor :data, :left, :right
     def initialize(data)
         @data = data
         @left = nil
@@ -9,23 +9,52 @@ class Node
 end
 
 class Tree
-    attr_accessor data:, root:
+    attr_accessor :data, :root
 
     def initialize(array)
         @data = array.sort.uniq
-        @root = buildTree(data)
+        @root = buildTree(array)
     end
 
-def buildTree(array)
-    if array.empty?
+    def buildTree(array)
+        if array.empty?
         return nil
-    
-    middle = (array.size - 1)/2
-    rootNode = Node.new(array[middle])
-    rootNode.left = buildTree(array[0...middle])
-    rootNode.right = buildTree(array[(middle+1)...])
+        end
+        middle = (array.size - 1)/2
+        rootNode = Node.new(array[middle])
+        rootNode.left = buildTree(array[0...middle])
+        rootNode.right = buildTree(array[(middle+1)...])
 
     return rootNode
-end
+    end
+
+    def insert(value, node = root)
+        if node.data == value
+            return nil
+        end
+
+        if value < node.data #recurse left
+            if node.left.nil?
+                node.left = Node.new(value)
+            else
+                insert(value, node.left)
+            end
+        end
+        
+        if value > node.data #recurse right
+            if node.right.nil?
+                node.right = Node.new(value)
+            else
+                insert(value,node.right)
+            end
+        end
+
+    end
 
 end
+
+array = [1,2,3,4,5,6,7]
+newtree = Tree.new(array)
+newtree.buildTree(array)
+newtree.insert(8)
+pp newtree
